1c4f8e49d200949b0e123818976dc686e399f305
Nick Mackenzie <mackenzie.nick@outlook.com>
2023-06-13 20:56:47 -0400

    layout for projects, import readme's from repo

---
+++ app/projects/souschef/page.tsx
@@ -0,0 +1,14 @@
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import Link from 'next/link'
+export const Page = async () => {
+  const res = await fetch("https://raw.githubusercontent.com/nickmackenzie/souschef/master/README.md")
+  const markdown = await res.text()
+  return (
+  <>
+  <MDXRemote source={markdown} />
+  <Link href='/projects'>Back</Link>
+  </>
+  )
+}
+
+export default Page;
